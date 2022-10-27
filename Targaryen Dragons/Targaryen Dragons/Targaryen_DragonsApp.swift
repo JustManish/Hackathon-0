46b4e2783ef0b1fe2111944d9612ec44005de5f9
Nasir Ahmed Momin <nasir.momin@mutualmobile.com>
2022-10-27 10:37:32 +0530

    Project creation

---
+++ Targaryen Dragons/Targaryen Dragons/Targaryen_DragonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Targaryen_DragonsApp.swift
+//  Targaryen Dragons
+//
+//  Created by Nasir Ahmed Momin on 27/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Targaryen_DragonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
