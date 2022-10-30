c3733d92c01ba4bda10fb981fb8e1fb38d13d592
Nasir Ahmed Momin <nasir.momin@mutualmobile.com>
2022-10-30 23:49:46 +0530

    Minor refactoring

--- Targaryen Dragons/Targaryen Dragons/ViewModels/LocationSearchViewModel.swift
+++ Targaryen Dragons/Targaryen Dragons/ViewModels/LocationSearchViewModel.swift
@@ -8,160 +8,163 @@
  import Foundation
  import MapKit
 
-//TODO: Remember Simulated Location is set from Wadala west to Gateway of India
+//Devs Remember Simulated Location is set from Wadala west to Gateway of India
 
- class LocationSearchViewModel: NSObject, ObservableObject {
+class LocationSearchViewModel: NSObject, ObservableObject {
+    
+    @Published var results = [MKLocalSearchCompletion]()
+    @Published var route: MKRoute?
+    @Published var selectedLocation: DragonLocation?
+    @Published var expectedArrivalTime: String?
+    @Published var lookAroundScene : MKLookAroundScene?
+    @Published var routeCoordinates: [CLLocationCoordinate2D] = []
+    
+    var timer: Timer?
+    var currentLocationIndex: Int = 0
+    var userLocation: CLLocationCoordinate2D?
 
-     @Published var results = [MKLocalSearchCompletion]()
-     @Published var route: MKRoute?
-     @Published var selectedLocation: DragonLocation?
-     @Published var expectedArrivalTime: String?
-     @Published var lookAroundScene : MKLookAroundScene?
-     @Published var routeCoordinates: [CLLocationCoordinate2D] = []
-     var tripStartedTime = Date()
-     let liveActivityManager = LiveActivityManager()
-     
-     var expectedTravelTimePerCoordinates: Double {
-         return totalExpectedTime / Double(routeCoordinates.count)
-     }
-     var totalTraveledTime: Double {
-         return expectedTravelTimePerCoordinates * Double(currentLocationIndex)
-     }
-     var totalExpectedTime: Double {
-         return route?.expectedTravelTime ?? 0.0
-     }
-     var routeSteps: [RouteStep] {
-         if let route {
+    var routeSteps: [RouteStep] {
+        if let route {
             return route.steps.map { RouteStep(step: $0) }
-         }
-         return []
-     }
-     var nextStep: MKRoute.Step? {
-         var calculatedTime = 0.0
-         for step in routeSteps{
-             if totalTraveledTime <= calculatedTime {
-                 return step.step
-             }
-             calculatedTime += expectedTimeForStep(step.step)
-         }
-         return nil
-     }
-     
-     var timer: Timer?
-     var currentLocationIndex: Int = 0
-     
-     private let searchCompleter = MKLocalSearchCompleter()
-     var queryFragment: String = "" {
-         didSet {
-             searchCompleter.queryFragment = queryFragment
-         }
-     }
-
-     var userLocation: CLLocationCoordinate2D?
-
-     override init() {
-         super.init()
-         searchCompleter.delegate = self
-         searchCompleter.queryFragment = queryFragment
-     }
-     
-     func incrementCurrentLocationIndex() {
-         if currentLocationIndex < routeCoordinates.count - 1 {
-             currentLocationIndex += 1
-         }
-     }
-     
-     func resetCurrentLocationIndex() {
-         currentLocationIndex = .zero
-     }
-
-     func selectLocation(_ localSearch: MKLocalSearchCompletion) {
-         locationSearch(forLocalSearchCompletion: localSearch) { response, error in
-             if let error = error {
-                 print("DEBUG: Location search failed with error \(error.localizedDescription)")
-                 return
-             }
-
-             guard let item = response?.mapItems.first else { return }
-             let coordinate = item.placemark.coordinate
-             self.selectedLocation = DragonLocation(title: localSearch.title,
-                                                      coordinate: coordinate,mapItem: item)
-             print("DEBUG: Location coordinates \(coordinate)")
-         }
-     }
-
-     func locationSearch(forLocalSearchCompletion localSearch: MKLocalSearchCompletion,
-                         completion: @escaping MKLocalSearch.CompletionHandler) {
-         let searchRequest = MKLocalSearch.Request()
-         searchRequest.naturalLanguageQuery = localSearch.title.appending(localSearch.subtitle)
-         let search = MKLocalSearch(request: searchRequest)
-         search.start(completionHandler: completion)
-     }
-
-     func getDestinationRoute(from userLocation: CLLocationCoordinate2D,
-                              to destination: CLLocationCoordinate2D,
-                              completion: @escaping(MKRoute) -> Void) {
-         let userPlacemark = MKPlacemark(coordinate: userLocation)
-         let destPlacemark = MKPlacemark(coordinate: destination)
-         let request = MKDirections.Request()
-         request.source = MKMapItem(placemark: userPlacemark)
-         request.destination = MKMapItem(placemark: destPlacemark)
-         let directions = MKDirections(request: request)
-         directions.calculate { response, error in
-             if let error = error {
-                 print("DEBUG: Failed to get directions with error \(error.localizedDescription)")
-                 return
-             }
-
-             guard let route = response?.routes.first else { return }
-             self.route = route
-             self.routeCoordinates = route.steps.flatMap(\.polyline.mkCoordinates)
-             // ----------- Code required to generate coordinates ---------
-             route.polyline.printGPXCoordinatesForRoute()
-             // ----------- Code required to generate coordinates ---------
-             self.getExpectedTravelTime(with: route.expectedTravelTime)
-             completion(route)
-         }
-     }
-
-     func getExpectedTravelTime(with expectedTravelTime: Double) {
-         let formatter = DateFormatter()
-         formatter.dateFormat = "hh:mm a"
-         expectedArrivalTime = formatter.string(from: Date() + expectedTravelTime)
-     }
-     
-     /// Gets the `MKLookAroundScene` for a map item after loading it asynchronously if necessary.
-     /// - parameter mapItem: The  map item.
-     func lookAroundScene(for mapItem: MKMapItem) {
-         let sceneRequest = MKLookAroundSceneRequest(mapItem: mapItem)
-         sceneRequest.getSceneWithCompletionHandler { scene, error in
-             if let error {
-                 print("Debug: \(mapItem.placemark.title ?? "")====\(error)")
-                 self.lookAroundScene = nil
-                 return
-             }
-             DispatchQueue.main.async {
-                 self.lookAroundScene = scene
-             }
-         }
-     }
-
-     func expectedTimeForStep(_ step: MKRoute.Step) -> Double {
-         if let route, totalExpectedTime != 0{
-             return (totalExpectedTime / route.distance) * step.distance
-         }
-         return 0.0
-     }
-     
-     func resetMap() {
-         LiveActivityManager().stop()
-         if let timer {
-             timer.invalidate()
-             self.timer = nil
-         }
-         lookAroundScene = nil
-     }
- }
+        }
+        return []
+    }
+    
+    var queryFragment: String = "" {
+        didSet {
+            searchCompleter.queryFragment = queryFragment
+        }
+    }
+    
+    private let liveActivityManager = LiveActivityManager()
+    private let searchCompleter = MKLocalSearchCompleter()
+    
+    private var expectedTravelTimePerCoordinates: Double {
+        return totalExpectedTime / Double(routeCoordinates.count)
+    }
+    
+    private var totalTraveledTime: Double {
+        return expectedTravelTimePerCoordinates * Double(currentLocationIndex)
+    }
+    
+    private var totalExpectedTime: Double {
+        return route?.expectedTravelTime ?? 0.0
+    }
+    
+    private var nextStep: MKRoute.Step? {
+        var calculatedTime = 0.0
+        for step in routeSteps{
+            if totalTraveledTime <= calculatedTime {
+                return step.step
+            }
+            calculatedTime += expectedTimeForStep(step.step)
+        }
+        return nil
+    }
+    
+    override init() {
+        super.init()
+        searchCompleter.delegate = self
+        searchCompleter.queryFragment = queryFragment
+    }
+    
+    func incrementCurrentLocationIndex() {
+        if currentLocationIndex < routeCoordinates.count - 1 {
+            currentLocationIndex += 1
+        }
+    }
+    
+    func resetCurrentLocationIndex() {
+        currentLocationIndex = .zero
+    }
+    
+    func selectLocation(_ localSearch: MKLocalSearchCompletion) {
+        locationSearch(forLocalSearchCompletion: localSearch) { response, error in
+            if let error = error {
+                print("DEBUG: Location search failed with error \(error.localizedDescription)")
+                return
+            }
+            
+            guard let item = response?.mapItems.first else { return }
+            let coordinate = item.placemark.coordinate
+            self.selectedLocation = DragonLocation(title: localSearch.title,
+                                                   coordinate: coordinate,mapItem: item)
+            print("DEBUG: Location coordinates \(coordinate)")
+        }
+    }
+    
+    func locationSearch(forLocalSearchCompletion localSearch: MKLocalSearchCompletion,
+                        completion: @escaping MKLocalSearch.CompletionHandler) {
+        let searchRequest = MKLocalSearch.Request()
+        searchRequest.naturalLanguageQuery = localSearch.title.appending(localSearch.subtitle)
+        let search = MKLocalSearch(request: searchRequest)
+        search.start(completionHandler: completion)
+    }
+    
+    func getDestinationRoute(from userLocation: CLLocationCoordinate2D,
+                             to destination: CLLocationCoordinate2D,
+                             completion: @escaping(MKRoute) -> Void) {
+        let userPlacemark = MKPlacemark(coordinate: userLocation)
+        let destPlacemark = MKPlacemark(coordinate: destination)
+        let request = MKDirections.Request()
+        request.source = MKMapItem(placemark: userPlacemark)
+        request.destination = MKMapItem(placemark: destPlacemark)
+        let directions = MKDirections(request: request)
+        directions.calculate { response, error in
+            if let error = error {
+                print("DEBUG: Failed to get directions with error \(error.localizedDescription)")
+                return
+            }
+            
+            guard let route = response?.routes.first else { return }
+            self.route = route
+            self.routeCoordinates = route.steps.flatMap(\.polyline.mkCoordinates)
+            // ----------- Code required to generate coordinates ---------
+            route.polyline.printGPXCoordinatesForRoute()
+            // ----------- Code required to generate coordinates ---------
+            self.getExpectedTravelTime(with: route.expectedTravelTime)
+            completion(route)
+        }
+    }
+    
+    func getExpectedTravelTime(with expectedTravelTime: Double) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "hh:mm a"
+        expectedArrivalTime = formatter.string(from: Date() + expectedTravelTime)
+    }
+    
+    /// Gets the `MKLookAroundScene` for a map item after loading it asynchronously if necessary.
+    /// - parameter mapItem: The  map item.
+    func lookAroundScene(for mapItem: MKMapItem) {
+        let sceneRequest = MKLookAroundSceneRequest(mapItem: mapItem)
+        sceneRequest.getSceneWithCompletionHandler { scene, error in
+            if let error {
+                print("Debug: \(mapItem.placemark.title ?? "")====\(error)")
+                self.lookAroundScene = nil
+                return
+            }
+            DispatchQueue.main.async {
+                self.lookAroundScene = scene
+            }
+        }
+    }
+    
+    func expectedTimeForStep(_ step: MKRoute.Step) -> Double {
+        if let route, totalExpectedTime != 0{
+            return (totalExpectedTime / route.distance) * step.distance
+        }
+        return 0.0
+    }
+    
+    func resetMap() {
+        LiveActivityManager().stop()
+        if let timer {
+            timer.invalidate()
+            self.timer = nil
+        }
+        lookAroundScene = nil
+    }
+}
 
  // MARK: - MKLocalSearchCompleterDelegate
 
