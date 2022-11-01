8616b64d522851fa02d62376ec4eccba0ebdfb1e
PatidarManish <102941982+PatidarManish@users.noreply.github.com>
2022-11-01 09:51:18 +0530

    Indentation fixed.

--- Targaryen Dragons/Targaryen Dragons/ViewModels/LocationSearchViewModel.swift
+++ Targaryen Dragons/Targaryen Dragons/ViewModels/LocationSearchViewModel.swift
@@ -20,31 +20,31 @@ class LocationSearchViewModel: NSObject, ObservableObject {
     var timer: Timer?
     var currentLocationIndex: Int = 0
     var userLocation: CLLocationCoordinate2D?
-
+    
     var routeSteps: [RouteStep] {
         if let route {
             return route.steps.map { RouteStep(step: $0) }
-         }
-         return []
-     }
+        }
+        return []
+    }
     
-     var nextStep: MKRoute.Step? {
-         var calculatedTime = 0.0
-         for step in routeSteps{
-             if totalTraveledTime <= calculatedTime {
-                 return step.step
-             }
-             calculatedTime += expectedTimeForStep(step.step)
-         }
-         return nil
-     }
-     
-     var queryFragment: String = "" {
-         didSet {
-             searchCompleter.queryFragment = queryFragment
-         }
-     }
-
+    var nextStep: MKRoute.Step? {
+        var calculatedTime = 0.0
+        for step in routeSteps{
+            if totalTraveledTime <= calculatedTime {
+                return step.step
+            }
+            calculatedTime += expectedTimeForStep(step.step)
+        }
+        return nil
+    }
+    
+    var queryFragment: String = "" {
+        didSet {
+            searchCompleter.queryFragment = queryFragment
+        }
+    }
+    
     private let liveActivityManager = LiveActivityManager()
     private let searchCompleter = MKLocalSearchCompleter()
     
@@ -56,91 +56,91 @@ class LocationSearchViewModel: NSObject, ObservableObject {
         return expectedTravelTimePerCoordinates * Double(currentLocationIndex)
     }
     
-     override init() {
-         super.init()
-         searchCompleter.delegate = self
-         searchCompleter.queryFragment = queryFragment
-     }
-     
-     func incrementCurrentLocationIndex() {
-         if currentLocationIndex < routeCoordinates.count - 1 {
-             currentLocationIndex += 1
-         }
-     }
-     
-     func resetCurrentLocationIndex() {
-         currentLocationIndex = .zero
-     }
-
-     func selectLocation(_ localSearch: MKLocalSearchCompletion) {
-         locationSearch(forLocalSearchCompletion: localSearch) { response, error in
-             if let error = error {
-                 print("DEBUG: Location search failed with error \(error.localizedDescription)")
-                 return
-             }
-
-             guard let item = response?.mapItems.first else { return }
-             let coordinate = item.placemark.coordinate
-             self.selectedLocation = DragonLocation(title: localSearch.title,
-                                                      coordinate: coordinate,mapItem: item)
-             print("DEBUG: Location coordinates \(coordinate)")
-         }
-     }
-
-     func locationSearch(forLocalSearchCompletion localSearch: MKLocalSearchCompletion,
-                         completion: @escaping MKLocalSearch.CompletionHandler) {
-         let searchRequest = MKLocalSearch.Request()
-         searchRequest.naturalLanguageQuery = localSearch.title.appending(localSearch.subtitle)
-         let search = MKLocalSearch(request: searchRequest)
-         search.start(completionHandler: completion)
-     }
-
-     func getDestinationRoute(from userLocation: CLLocationCoordinate2D,
-                              to destination: CLLocationCoordinate2D,
-                              completion: @escaping(MKRoute) -> Void) {
-         let userPlacemark = MKPlacemark(coordinate: userLocation)
-         let destPlacemark = MKPlacemark(coordinate: destination)
-         let request = MKDirections.Request()
-         request.source = MKMapItem(placemark: userPlacemark)
-         request.destination = MKMapItem(placemark: destPlacemark)
-         let directions = MKDirections(request: request)
-         directions.calculate { response, error in
-             if let error = error {
-                 print("DEBUG: Failed to get directions with error \(error.localizedDescription)")
-                 return
-             }
-
-             guard let route = response?.routes.first else { return }
-             self.route = route
-             self.routeCoordinates = route.steps.flatMap(\.polyline.mkCoordinates)
-             route.polyline.printGPXCoordinatesForRoute()
-             self.getExpectedTravelTime(with: route.expectedTravelTime)
-             completion(route)
-         }
-     }
-
-     func getExpectedTravelTime(with expectedTravelTime: Double) {
-         let formatter = DateFormatter()
-         formatter.dateFormat = "hh:mm a"
-         expectedArrivalTime = formatter.string(from: Date() + expectedTravelTime)
-     }
-     
-     /// Gets the `MKLookAroundScene` for a map item after loading it asynchronously if necessary.
-     /// - parameter mapItem: The  map item.
-     func lookAroundScene(for mapItem: MKMapItem) {
-         let sceneRequest = MKLookAroundSceneRequest(mapItem: mapItem)
-         sceneRequest.getSceneWithCompletionHandler { scene, error in
-             if let error {
-                 print("Debug: \(mapItem.placemark.title ?? "")====\(error)")
-                 self.lookAroundScene = nil
-                 return
-             }
-             DispatchQueue.main.async {
-                 self.lookAroundScene = scene
-             }
-         }
-     }
-
+    override init() {
+        super.init()
+        searchCompleter.delegate = self
+        searchCompleter.queryFragment = queryFragment
+    }
+    
+    func incrementCurrentLocationIndex() {
+        if currentLocationIndex < routeCoordinates.count - 1 {
+            currentLocationIndex += 1
+        }
+    }
+    
+    func resetCurrentLocationIndex() {
+        currentLocationIndex = .zero
+    }
+    
+    func selectLocation(_ localSearch: MKLocalSearchCompletion) {
+        locationSearch(forLocalSearchCompletion: localSearch) { response, error in
+            if let error = error {
+                print("DEBUG: Location search failed with error \(error.localizedDescription)")
+                return
+            }
+            
+            guard let item = response?.mapItems.first else { return }
+            let coordinate = item.placemark.coordinate
+            self.selectedLocation = DragonLocation(title: localSearch.title,
+                                                   coordinate: coordinate,mapItem: item)
+            print("DEBUG: Location coordinates \(coordinate)")
+        }
+    }
+    
+    func locationSearch(forLocalSearchCompletion localSearch: MKLocalSearchCompletion,
+                        completion: @escaping MKLocalSearch.CompletionHandler) {
+        let searchRequest = MKLocalSearch.Request()
+        searchRequest.naturalLanguageQuery = localSearch.title.appending(localSearch.subtitle)
+        let search = MKLocalSearch(request: searchRequest)
+        search.start(completionHandler: completion)
+    }
+    
+    func getDestinationRoute(from userLocation: CLLocationCoordinate2D,
+                             to destination: CLLocationCoordinate2D,
+                             completion: @escaping(MKRoute) -> Void) {
+        let userPlacemark = MKPlacemark(coordinate: userLocation)
+        let destPlacemark = MKPlacemark(coordinate: destination)
+        let request = MKDirections.Request()
+        request.source = MKMapItem(placemark: userPlacemark)
+        request.destination = MKMapItem(placemark: destPlacemark)
+        let directions = MKDirections(request: request)
+        directions.calculate { response, error in
+            if let error = error {
+                print("DEBUG: Failed to get directions with error \(error.localizedDescription)")
+                return
+            }
+            
+            guard let route = response?.routes.first else { return }
+            self.route = route
+            self.routeCoordinates = route.steps.flatMap(\.polyline.mkCoordinates)
+            route.polyline.printGPXCoordinatesForRoute()
+            self.getExpectedTravelTime(with: route.expectedTravelTime)
+            completion(route)
+        }
+    }
+    
+    func getExpectedTravelTime(with expectedTravelTime: Double) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "hh:mm a"
+        expectedArrivalTime = formatter.string(from: Date() + expectedTravelTime)
+    }
+    
+    /// Gets the `MKLookAroundScene` for a map item after loading it asynchronously if necessary.
+    /// - parameter mapItem: The  map item.
+    func lookAroundScene(for mapItem: MKMapItem) {
+        let sceneRequest = MKLookAroundSceneRequest(mapItem: mapItem)
+        sceneRequest.getSceneWithCompletionHandler { scene, error in
+            if let error {
+                print("Debug: \(mapItem.placemark.title ?? "")====\(error)")
+                self.lookAroundScene = nil
+                return
+            }
+            DispatchQueue.main.async {
+                self.lookAroundScene = scene
+            }
+        }
+    }
+    
     private var totalExpectedTime: Double {
         return route?.expectedTravelTime ?? 0.0
     }
@@ -151,19 +151,20 @@ class LocationSearchViewModel: NSObject, ObservableObject {
         }
         return 0.0
     }
-     
-     func resetMap() {
-         stopNavigation()
-         lookAroundScene = nil
-     }
-     func updatePropertiesOnTimer() {
-         currentLocationIndex == .zero ? startLiveActivity() : updateLiveActivity()
-         incrementCurrentLocationIndex()
-         if currentLocationIndex == routeCoordinates.count - 1 {
-             stopNavigation()
-             resetCurrentLocationIndex()
-         }
-     }
+    
+    func resetMap() {
+        stopNavigation()
+        lookAroundScene = nil
+    }
+    
+    func updatePropertiesOnTimer() {
+        currentLocationIndex == .zero ? startLiveActivity() : updateLiveActivity()
+        incrementCurrentLocationIndex()
+        if currentLocationIndex == routeCoordinates.count - 1 {
+            stopNavigation()
+            resetCurrentLocationIndex()
+        }
+    }
     
     func stopNavigation() {
         timer?.invalidate()
@@ -171,14 +172,13 @@ class LocationSearchViewModel: NSObject, ObservableObject {
         resetCurrentLocationIndex()
         endLiveActivity()
     }
-     
-     func checkAndDismissLiveActivity() {
-         if let _ = timer {
-             endLiveActivity()
-         }
-     }
-     
- }
+    
+    func checkAndDismissLiveActivity() {
+        if let _ = timer {
+            endLiveActivity()
+        }
+    }
+}
 
  // MARK: - MKLocalSearchCompleterDelegate
 
--- Targaryen Dragons/Targaryen Dragons/Views/LocationSearchListView.swift
+++ Targaryen Dragons/Targaryen Dragons/Views/LocationSearchListView.swift
@@ -7,67 +7,67 @@
 
  import SwiftUI
 
- struct LocationSearchListView: View {
-     @State private var startLocationText = ""
-     @Binding var mapState: MapViewState
-     @EnvironmentObject var viewModel: LocationSearchViewModel
-
-     var body: some View {
-         VStack {
-             HStack {
-                 VStack {
-                     Circle()
-                         .fill(Color(.systemGray3))
-                         .frame(width: 6, height: 6)
-
-                     Rectangle()
-                         .fill(Color(.systemGray3))
-                         .frame(width: 1, height: 24)
-
-                     Rectangle()
-                         .fill(Color.gray.opacity(0.5))
-                         .frame(width: 6, height: 6)
-                 }
-
-                 VStack {
-                     TextField("Current location", text: $startLocationText)
-                         .frame(height: 32)
-                         .background(Color(.systemGroupedBackground))
-                         .padding(.trailing)
-                         .disabled(true)
-
-                     TextField("Where to?", text: $viewModel.queryFragment)
-                         .frame(height: 32)
-                         .foregroundColor(.black)
-                         .background(Color(.white))
-                         .padding(.trailing)
-                 }
-             }
-             .padding(.horizontal)
-             .padding(.top, 64)
-
-             Divider()
-                 .padding(.vertical)
-
-             // list view
-             ScrollView {
-                 LazyVStack(alignment: .leading) {
-                     ForEach(viewModel.results, id: \.self) { result in
-                         LocationSearchResultView(title: result.title, subtitle: result.subtitle)
-                             .onTapGesture {
-                                 withAnimation(.spring()) {
-                                     viewModel.selectLocation(result)
-                                     mapState = .locationSelected
-                                 }
-                             }
-                     }
-                 }
-             }
-         }
-         .background(Color.gray.opacity(0.5))
-         .background(.white)
-     }
- }
+struct LocationSearchListView: View {
+    @State private var startLocationText = ""
+    @Binding var mapState: MapViewState
+    @EnvironmentObject var viewModel: LocationSearchViewModel
+    
+    var body: some View {
+        VStack {
+            HStack {
+                VStack {
+                    Circle()
+                        .fill(Color(.systemGray3))
+                        .frame(width: 6, height: 6)
+                    
+                    Rectangle()
+                        .fill(Color(.systemGray3))
+                        .frame(width: 1, height: 24)
+                    
+                    Rectangle()
+                        .fill(Color.gray.opacity(0.5))
+                        .frame(width: 6, height: 6)
+                }
+                
+                VStack {
+                    TextField("Current location", text: $startLocationText)
+                        .frame(height: 32)
+                        .background(Color(.systemGroupedBackground))
+                        .padding(.trailing)
+                        .disabled(true)
+                    
+                    TextField("Where to?", text: $viewModel.queryFragment)
+                        .frame(height: 32)
+                        .foregroundColor(.black)
+                        .background(Color(.white))
+                        .padding(.trailing)
+                }
+            }
+            .padding(.horizontal)
+            .padding(.top, 64)
+            
+            Divider()
+                .padding(.vertical)
+            
+            // list view
+            ScrollView {
+                LazyVStack(alignment: .leading) {
+                    ForEach(viewModel.results, id: \.self) { result in
+                        LocationSearchResultView(title: result.title, subtitle: result.subtitle)
+                            .onTapGesture {
+                                withAnimation(.spring()) {
+                                    viewModel.selectLocation(result)
+                                    mapState = .locationSelected
+                                }
+                            }
+                    }
+                }
+            }
+        }
+        .background(Color.gray.opacity(0.5))
+        .background(.white)
+    }
+}
 
  struct LocationSearchView_Previews: PreviewProvider {
      static var previews: some View {
